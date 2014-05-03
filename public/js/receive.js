b2c1eac20d47ed138406666668073aa52ef48e27
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-05-03 18:54:06 +0900

    fix some files

--- public/js/receive.js
+++ public/js/receive.js
@@ -1,6 +1,7 @@
 var peer = new Peer('receiverID', {key: '7kov9dhv87fa8aor'});
 peer.on('connection', function(conn){
   conn.on('data', function(data) {
-    document.getElementById("message").innerHTML = data.replace('/n', '<br />');
+    document.getElementById("message").innerHTML = data;
   });
 });
+
