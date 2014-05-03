cfc12b86186661f914cdde46e3f71e0db727b8f9
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-05-03 17:11:58 +0900

    fix some files / ドロップしたファイル内容の送信成功

--- public/js/send.js
+++ public/js/send.js
@@ -1,5 +1,40 @@
-var peer = new Peer('senderID',{key: 'uoux7azk7sxg8pvi'});
-var conn = peer.connect('receiverID');
-conn.on('open',function(){
-  conn.send('hi');
+/// utilを設定
+var util = {
+  stop: function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  },
+}
+
+$(function() {
+  /* peer setup */
+  var peer = new Peer('senderID',{key: 'uoux7azk7sxg8pvi'});
+  /* jquery setup */
+  var $dropArea = $('#dropable');
+  /* some param */
+  var fn = "", fs = "", fv = "";
+
+  $dropArea.on({
+    'dragenter' : util.stop,
+    'dragover' : util.stop,
+    'dragleave' : util.stop,
+    'drop': function(e) {
+      util.stop(e);
+      var conn = peer.connect('receiverID');
+      var tmp = e.originalEvent.dataTransfer.files;
+      var files = Array.prototype.slice.call(tmp, 0, tmp.length);
+      files.forEach(function(file) {
+        var reader = new FileReader();
+        $(reader).one('load', function(e) {
+          fn = file.name;
+          fs = file.size;
+          fv = e.target.result;
+        });
+        reader.readAsText(file);
+        conn.on('open',function(){
+          conn.send(fv);
+        });
+      });
+    }
+  });
 });
