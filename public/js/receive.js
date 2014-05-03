996922e024a4b4a3d6fb46e18b73f017f8dcd306
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-05-03 15:29:53 +0900

    fix some files / 文字列の通信に成功

--- public/js/receive.js
+++ public/js/receive.js
@@ -1,4 +1,6 @@
 var peer = new Peer('receiverID', {key: 'uoux7azk7sxg8pvi'});
-peer.on('data', function(data) {
+peer.on('connection', function(conn){
+  conn.on('data', function(data) {
     document.getElementById("message").innerHTML = data;
+  });
 });
